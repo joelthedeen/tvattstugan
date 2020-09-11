72047fcfe46ff961a5dedaba34ed22a0600fa28a
Joel Thedéen <joelthedeen@gmail.com>
2020-09-11 10:58:59 +0200

    Tvattstugan  skiss

---
+++ SecondScreen.swift
@@ -0,0 +1,29 @@
+//
+//  SecondScreen.swift
+//  Tvattstugan
+//
+//  Created by Joel Thedéen on 2020-09-10.
+//
+
+import UIKit
+
+class SecondScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Tvattstugan/ViewController.swift
+++ Tvattstugan/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var textFieldBRF: UITextField!
+    
+    @IBOutlet weak var textFieldLGH: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+  
+        
+    
 
+  
+  
+    let bottomLine = CALayer()
+        
+        bottomLine.frame = CGRect(x: 0, y: textFieldBRF.frame.height - 1, width: textFieldBRF.frame.width, height: 1)
+        
+        
+        bottomLine.backgroundColor = UIColor.init(red: 255/255, green: 255/255, blue: 255/255, alpha: 1).cgColor
+        
+    textFieldBRF.borderStyle = .none
+        
+    textFieldBRF.layer.addSublayer(bottomLine)
 
+}
 }
 
